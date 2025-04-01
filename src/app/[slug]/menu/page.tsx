7f2198a6816eacf8782b368dd1e03c6097e9ed06
ramoncodevale <ramonvalentim88@gmail.com>
2025-04-01 15:12:14 -0300

    fix: fixing the component

--- src/app/[slug]/menu/page.tsx
+++ src/app/[slug]/menu/page.tsx
@@ -1,49 +1,46 @@
-import { db } from "@/lib/prisma";
 import { notFound } from "next/navigation";
-import RestaurantHeader from "./components/header";
+
+import { db } from "@/lib/prisma";
+
 import RestaurantCategories from "./components/categories";
+import RestaurantHeader from "./components/header";
 
 interface RestaurantMenuPageProps {
-  params?: { slug?: string };
-  searchParams?: { consumptionMethod?: string };
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
 }
 
-const isConsumptionMethodValid = (consumptionMethod?: string) => {
-  return !consumptionMethod || ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN", "TAKEAWAY"].includes(consumptionMethod.toUpperCase());
 };
 
-const RestaurantMenuPage = async ({ params, searchParams }: RestaurantMenuPageProps) => {
-  const slug = params?.slug || "";
-  const consumptionMethod = searchParams?.consumptionMethod || "";
-
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
   if (!isConsumptionMethodValid(consumptionMethod)) {
     return notFound();
   }
-
-  try {
-    const restaurant = await db.restaurant.findUnique({
-      where: { slug },
-      include: {
-        menuCategories: {
-          include: { products: true },
-        },
+  const restaurant = await db.restaurant.findUnique({
+    where: { slug },
+    include: {
+      menuCategories: {
+        include: { products: true },
       },
-    });
-
-    if (!restaurant) {
-      return notFound();
-    }
-
-    return (
-      <div>
-        <RestaurantHeader restaurant={restaurant} />
-        <RestaurantCategories restaurant={restaurant} />
-      </div>
-    );
-  } catch (error) {
-    console.error("Erro ao buscar restaurante:", error);
+    },
+  });
+  if (!restaurant) {
     return notFound();
   }
+  return (
+    <div>
+      <RestaurantHeader restaurant={restaurant} />
+      <RestaurantCategories restaurant={restaurant} />
+    </div>
+  );
 };
 
 export default RestaurantMenuPage;
+
